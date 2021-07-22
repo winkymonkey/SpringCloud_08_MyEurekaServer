44a949a2724b99ac66b6a345dd5689c124cc2179
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-22 11:49:36 +0530

    refactor

--- src/main/java/org/example/demo/MyEurekaServerApplication.java
+++ src/main/java/org/example/demo/MyEurekaServerApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package org.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+
 @SpringBootApplication
 @EnableEurekaServer
 public class MyEurekaServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyEurekaServerApplication.class, args);
 	}
